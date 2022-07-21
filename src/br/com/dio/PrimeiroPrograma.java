cea1d7f4cc35e3291893921c9f543fa3e52cf7b1
Silvana <silvanwoliveira@gmail.com>
2022-07-21 14:10:26 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+import java.sql.SQLOutput;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        gato gato = new gato();
+        System.out.println(gato);
+
+
+       /* int a = 5;
+        int b =3 ;
+
+        System.out.println("Hello world"+ (a+b));*/
+    }
+}
